39711af76c819b7da5f40bfa98473e4adf5274a0
dinhngocduc1311 <22028166@vnu.edu.vn>
2025-03-31 20:25:54 +0700

    Create Stepper3.cy.jsx

---
+++ src/components/Stepper3.cy.jsx
@@ -0,0 +1,17 @@
+import Stepper from "./Stepper";
+
+describe("<Stepper>", () => {
+  it("mounts", () => {
+    cy.mount(<Stepper />);
+  });
+
+  it("stepper should default to 0", () => {
+    cy.mount(<Stepper />);
+    cy.get("[data-cy=counter]").should("have.text", "0");
+  });
+
+  it('supports a "count" prop to set the value', () => {
+    cy.mount(<Stepper count={100} />);
+    cy.get("[data-cy=counter]").should("have.text", "100");
+  });
+});
