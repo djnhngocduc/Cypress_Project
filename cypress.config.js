3b0b74bba12fffa0011445ac101cd0c1484ce5a8
dinhngocduc1311 <22028166@vnu.edu.vn>
2025-03-31 19:18:09 +0700

    create example cypress cloud

---
+++ cypress.config.js
@@ -0,0 +1,19 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+      indexHtmlFile: 'cypress/support/component-index.html'
+    },
+    specPattern: 'src/**/*.cy.{js,jsx,ts,tsx}'
+  },
+  projectId: "sdbxxk"
+});
