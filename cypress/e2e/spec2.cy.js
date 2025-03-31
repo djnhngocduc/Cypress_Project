0bfd84f3fc148e56ead06ec121b4399b7c269c0a
dinhngocduc1311 <22028166@vnu.edu.vn>
2025-03-31 20:10:54 +0700

    Create spec2.cy.js

---
+++ cypress/e2e/spec2.cy.js
@@ -0,0 +1,10 @@
+describe('Login Test', () => {
+  it('Login with valid credentials', () => {
+    cy.get('[data-test="username"]').type('standard_user'); // Nhập tên người dùng
+    cy.get('[data-test="password"]').type('secret_sauce'); // Nhập mật khẩu
+    cy.get('[data-test="login-button"]').click(); // Nhấn nút đăng nhập
+
+    // Kiểm tra xem đã đăng nhập thành công hay chưa
+    cy.url().should('include', '/inventory.html');
+  });
+});
