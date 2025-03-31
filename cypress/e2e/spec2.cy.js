dab758995ffc6f46828ef2433fe34ff3e10be756
dinhngocduc1311 <22028166@vnu.edu.vn>
2025-03-31 20:19:53 +0700

    update spec2.cy.js

--- cypress/e2e/spec2.cy.js
+++ cypress/e2e/spec2.cy.js
@@ -1,4 +1,8 @@
 describe('Login Test', () => {
+  beforeEach(() => {
+    cy.visit('https://www.saucedemo.com'); // Điều hướng đến trang đăng nhập
+  });
+  
   it('Login with valid credentials', () => {
     cy.get('[data-test="username"]').type('standard_user'); // Nhập tên người dùng
     cy.get('[data-test="password"]').type('secret_sauce'); // Nhập mật khẩu
