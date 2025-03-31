4af0c2794eab561111d43665b82fdabbc9560afc
dinhngocduc1311 <22028166@vnu.edu.vn>
2025-03-31 20:39:16 +0700

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
   component: {
     devServer: {
       framework: "react",
@@ -8,12 +13,6 @@ module.exports = defineConfig({
       indexHtmlFile: 'cypress/support/component-index.html'
     },
     specPattern: 'src/**/*.cy.{js,jsx,ts,tsx}'
-  },
-
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
   },
   projectId: "sdbxxk"
 });
